81530a50cc1088d48c8f2748fb009c1de6b78ea3
sefacakir <cakirsefa72@gmail.com>
2021-12-01 20:55:54 +0300

    Search function added

--- HashTable/LinearProbing.cs
+++ HashTable/LinearProbing.cs
@@ -7,13 +7,16 @@ namespace HashTable
     class LinearProbing
     {
         DataItem[] dataItems;
-        int counter = 0;
+        int counter;
         public LinearProbing()
         {
             dataItems = new DataItem[20];
         }
+
+
         public void Insert(DataItem dataItem)
         {
+            counter = 0;
             if (counter == dataItems.Length)
             {
                 Console.WriteLine("Table Full!!");
@@ -27,10 +30,10 @@ namespace HashTable
             }
             else
             {
-                while(dataItems[temp] != null)
+                while (dataItems[temp] != null)
                 {
                     temp++; //her dolu olduğunda bir sonrakine geçip baksın.
-                    if (temp > dataItems.Length-1)
+                    if (temp > dataItems.Length - 1)
                     {
                         temp = 0;
                     }
@@ -39,14 +42,42 @@ namespace HashTable
                 counter++;
             }
         }
+
         public void List()
         {
             Console.WriteLine("index, key, value\n-------------------------------");
             for (int i = 0; i < dataItems.Length; i++)
             {
-                if(dataItems[i]!=null)
-                Console.WriteLine(i+"   "+dataItems[i].Key+"   " +dataItems[i].Value);
+                if (dataItems[i] != null)
+                    Console.WriteLine(i + "   " + dataItems[i].Key + "   " + dataItems[i].Value);
             }
+        }
+
+        public DataItem Search(DataItem dataItem)
+        {
+            counter = 0;
+            int temp = dataItem.Key % 20;
+            if (dataItems[temp] == null)
+            {
+                Console.WriteLine("Search not found!!");
+                return null;
+            }
+            
+            while (dataItems[temp].Key != dataItem.Key)
+            {
+                temp++;
+                if (temp > dataItems.Length - 1)
+                {
+                    temp %= 20;
+                }
+                if (counter == 20 || dataItems[temp]==null)
+                {
+                    Console.WriteLine("Search not found.");
+                    return null;
+                }
+                counter++;
+            }
+            return dataItems[temp];
         }
     }
 }
--- HashTable/Program.cs
+++ HashTable/Program.cs
@@ -14,8 +14,13 @@ namespace HashTable
             linearProbing.Insert(new DataItem { Key = 7, Value = 4 });
             linearProbing.Insert(new DataItem { Key = 18, Value = 5 });
             linearProbing.Insert(new DataItem { Key = 19, Value = 6 });
-            linearProbing.Insert(new DataItem { Key = 19, Value = 7 });
+            linearProbing.Insert(new DataItem { Key = 39, Value = 7 });
             linearProbing.List();
+
+
+            Console.WriteLine(linearProbing.Search(new DataItem { Key = 39, Value = 7 }).Key);
+            Console.WriteLine(linearProbing.Search(new DataItem { Key = 40, Value = 7 }));
+
 
 
 

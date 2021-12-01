f2928dc01c695361a133cf3b7a8ac00a9d7c0392
sefacakir <cakirsefa72@gmail.com>
2021-12-01 14:36:17 +0300

    Queue added.

---
+++ Queue/ArrayQueuFunctions.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Queue
+{
+    class ArrayQueuFunctions
+    {
+        int[] array;
+        public ArrayQueuFunctions()
+        {
+            array = new int[0];
+        }
+        public bool isEmpty()
+        {
+            if (array.Length == 0)
+            {
+                return true;
+            }
+            else return false;
+        }
+        public void Enqueue(int value)
+        {
+            int[] tempArray = new int[array.Length + 1];
+            for (int i = 0; i < array.Length; i++)
+            {
+                tempArray[i] = array[i];
+            }
+            tempArray[array.Length] = value;
+            array = tempArray;
+        }
+        public int Dequeue()
+        {
+            if (isEmpty())
+            {
+                return -1;
+            }
+
+            int temp = array[array.Length - 1];
+            int[] tempArray = new int[array.Length - 1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                tempArray[i] = array[i+1];
+            }
+            array = tempArray;
+            return temp;
+        }
+
+        public void List()
+        {
+            foreach (var item in array)
+            {
+                Console.Write(item+"  ");
+            }
+            Console.WriteLine("\n");
+        }
+    }
+}
---
+++ Queue/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayQueuFunctions functions = new ArrayQueuFunctions();
+            functions.Enqueue(5);
+            functions.Enqueue(15);
+            functions.Enqueue(8);
+            functions.Enqueue(20);
+            functions.List();
+
+            functions.Dequeue();
+            functions.Dequeue();
+            functions.List();
+
+            functions.Enqueue(35);
+            functions.Enqueue(45);
+            functions.Enqueue(55);
+            functions.List();
+            
+            functions.Dequeue();
+            functions.Dequeue();
+            functions.List();
+        }
+    }
+}
